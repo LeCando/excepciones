2e8eb038df816949a721f7df8c58c09b27c6b067
luis.cando <luiseduardo.cando@udla.edu.ec>
2024-01-14 14:08:53 -0500

    Initial commit

---
+++ src/Excepcion.java
@@ -0,0 +1,14 @@
+public class Excepciones {
+    public void excepcionNullPointer(){
+
+    }
+    public void excepcionNumberFormat(){
+
+    }
+
+    public void excepcionFileNotFound(){
+
+    }
+
+
+}
